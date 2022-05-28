12f6344c1337185a8dfc846c536f6bdb30e4c918
coisini <18721966209@163.com>
2022-05-28 19:16:05 +0800

    新增员工

--- src/main/java/com/qf/reggie/controller/EmployController.java
+++ src/main/java/com/qf/reggie/controller/EmployController.java
@@ -7,9 +7,13 @@ import com.qf.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -53,6 +57,7 @@ public class EmployController {
 
     /**
      * 员工退出
+     *
      * @param request
      * @return
      */
@@ -61,5 +66,26 @@ public class EmployController {
         //清理Session中保存的当前登录员工的id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+    /**
+     * 新增员工
+     *
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Employee employee, HttpServletRequest request) {
+        log.info("新增员工，员工信息：{}", employee.toString());
+        //设置初始密码123456，需要进行md5的加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        //获取当前用户登录的id
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+        employeeService.save(employee);
+        return R.success("新增员工成功");
     }
 }
