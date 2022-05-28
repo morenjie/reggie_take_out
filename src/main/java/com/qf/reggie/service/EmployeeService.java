31e59e7e48b9b900b030b5e10386fe53005f9cc1
coisini <18721966209@163.com>
2022-05-28 14:12:04 +0800

    完成登录、注册功能

---
+++ src/main/java/com/qf/reggie/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.qf.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.qf.reggie.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
