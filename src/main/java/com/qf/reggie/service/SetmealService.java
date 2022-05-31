923ad52792bb4d22f7e6ac685fc3fe73fd22036d
coisini <18721966209@163.com>
2022-05-31 14:47:10 +0800

    删除套餐

--- src/main/java/com/qf/reggie/service/SetmealService.java
+++ src/main/java/com/qf/reggie/service/SetmealService.java
@@ -4,10 +4,19 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.qf.reggie.dto.SetmealDto;
 import com.qf.reggie.entity.Setmeal;
 
+import java.util.List;
+
 public interface SetmealService extends IService<Setmeal> {
     /**
-     *  新增套餐，同时需要保存套餐和菜品的关联信息
+     * 新增套餐，同时需要保存套餐和菜品的关联信息
+     *
      * @param setmealDto
      */
     public void saveWithDish(SetmealDto setmealDto);
+
+    /**
+     * 删除套餐，同时需要删除套餐和菜品的关联数据
+     * @param ids
+     */
+    public void removeWithDish(List<Long> ids);
 }
--- src/main/java/com/qf/reggie/service/impl/SetmealServiceImpl.java
+++ src/main/java/com/qf/reggie/service/impl/SetmealServiceImpl.java
@@ -1,6 +1,8 @@
 package com.qf.reggie.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.qf.reggie.common.CustomException;
 import com.qf.reggie.dto.SetmealDto;
 import com.qf.reggie.entity.Setmeal;
 import com.qf.reggie.entity.SetmealDish;
@@ -38,5 +40,29 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
         }).collect(Collectors.toList());
         //保存套餐和菜品的关联信息，操作setmeal_dish,执行insert操作
         setmealDishService.saveBatch(setmealDishes);
+    }
+
+    /**
+     * 删除套餐，同时需要删除套餐和菜品的关联数据
+     *
+     * @param ids
+     */
+    @Override
+    public void removeWithDish(List<Long> ids) {
+        //查询套餐状态，确定是否可用删除
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper();
+        queryWrapper.in(Setmeal::getId, ids);
+        queryWrapper.eq(Setmeal::getStatus, 1);
+        int count = this.count(queryWrapper);
+        if (count > 0) {
+            //如果不能删除，抛出一个业务异常
+            throw new CustomException("套餐正在售卖中，不能删除");
+        }
+        //如果可以删除，先删除套餐表里的数据---setmeal
+        this.removeByIds(ids);
+        //删除关系表中的数据---setmeal_dish
+       LambdaQueryWrapper<SetmealDish>lambdaQueryWrapper = new LambdaQueryWrapper<>();
+       lambdaQueryWrapper.in(SetmealDish::getSetmealId,ids);
+       setmealDishService.remove(lambdaQueryWrapper);
     }
 }
