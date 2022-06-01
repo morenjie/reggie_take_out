bb2f8ff8fbf2d1d60b4dcd1c902f31469e987cd0
coisini <18721966209@163.com>
2022-06-01 14:34:38 +0800

    查看购物车、清空购物车

--- src/main/java/com/qf/reggie/controller/ShoppingCartController.java
+++ src/main/java/com/qf/reggie/controller/ShoppingCartController.java
@@ -7,10 +7,10 @@ import com.qf.reggie.entity.ShoppingCart;
 import com.qf.reggie.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/shoppingCart")
@@ -52,9 +52,38 @@ public class ShoppingCartController {
         } else {
             //如果不存在，则添加到购物车，数量默认是1
             shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
             shoppingCartService.save(shoppingCart);
-            cartServiceOne=shoppingCart;
+            cartServiceOne = shoppingCart;
         }
         return R.success(cartServiceOne);
+    }
+
+    /**
+     * 查看购物车
+     *
+     * @return
+     */
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> list() {
+        log.info("查看购物车...");
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        queryWrapper.orderByAsc(ShoppingCart::getCreateTime);
+        List<ShoppingCart> list = shoppingCartService.list(queryWrapper);
+        return R.success(list);
+    }
+
+    /**
+     * 清空购物车
+     *
+     * @return
+     */
+    @DeleteMapping("/clean")
+    public R<String> clean() {
+        LambdaQueryWrapper<ShoppingCart> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(ShoppingCart::getUserId, BaseContext.getCurrentId());
+        shoppingCartService.remove(queryWrapper);
+        return R.success("清空购物车成功");
     }
 }
